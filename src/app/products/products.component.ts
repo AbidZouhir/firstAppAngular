66536f5c4c3acea966914f394f8dcb563f6ce485
zouhir <zouhir.etransit@gmail.com>
2024-01-26 18:33:51 +0100

    second commit

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductService} from "../services/product.service";
 import {Product} from "../model/product.model";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-product',
@@ -8,15 +9,31 @@ import {Product} from "../model/product.model";
   styleUrl: './products.component.css'
 })
 export class ProductsComponent implements OnInit{
-  products : Array<Product> = [];
+  public products : Array<Product>=[];
+  public keyword : string="";
+  totalPages : number=0;
+  pageSize : number=4
+  currentPage : number=1;
   constructor(private productService : ProductService) {
   }
 
   ngOnInit(){
-        this.productService.getProducts().subscribe({
-          next : data => this.products=data,
-          error : err => console.log(err)
-        })
+     this.getProducts();
+    }
+    getProducts(){
+      this.productService.getProducts(this.currentPage,this.pageSize).subscribe({
+        next : (response) => {
+          this.products=response.body as Product[];
+          let totalProduct : number=parseInt(response.headers.get(`X-Total-Count`)!);
+          this.totalPages=Math.floor(totalProduct/this.pageSize);
+          if (totalProduct % this.pageSize != 0){
+            this.totalPages++;
+          }
+          console.log(this.totalPages)
+        },
+        error : err => console.log(err)
+      })
+      //this.products=this.productService.getProducts();
     }
 
 
@@ -27,5 +44,26 @@ export class ProductsComponent implements OnInit{
           product.checked=!product.checked;
         }
       })
+  }
+
+  deleteProduct(product: Product) {
+    if (confirm("Etes vous Sûre?"))
+    this.productService.deleteProduct(product).subscribe({
+      next : value => {
+        //this.getProducts();
+        this.products=this.products.filter(p=>p.id!=product.id);
+      }
+    });
+  }
+
+  searchProducts() {
+    this.productService.searchProduct(this.keyword).subscribe({
+      next : value => this.products=value
+    })
+  }
+
+  goToPage(page: number) {
+    this.currentPage=page;
+    this.getProducts();
   }
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,10 +9,19 @@ import {Product} from "../model/product.model";
 export class ProductService {
 
   constructor(private http : HttpClient) { }
-  public getProducts():Observable<Array<Product>>{
-    return this.http.get<Array<Product>>("http://localhost:8089/products");
+  public getProducts(page : number=1, size : number=4){
+    return this.http.get(`http://localhost:8089/products?_page=${page}&_limit=${size}`,{observe:`response`});
   }
   public checkProduct(product : Product):Observable<Product>{
     return this.http.patch<Product>(`http://localhost:8089/products/${product.id}`, {checked :! product.checked});
+  }
+  public deleteProduct(product : Product){
+    return this.http.delete<any>(`http://localhost:8089/products/${product.id}`);
+  }
+  public saveProduct(product : Product):Observable<Product>{
+    return this.http.post<Product>(`http://localhost:8089/products`, product);
+  }
+  public searchProduct(keyword : String):Observable<Array<Product>>{
+    return this.http.get<Array<Product>>(`http://localhost:8089/products?name_like=${keyword}`);
   }
 }
